cefd754f0adf6f52c0126e985a766de2052944ec
aniversarioperu <aniversarioperu1@gmail.com>
2015-08-18 09:46:41 +0300

    more test cases

--- manolo_scraper/tests/test_utils.py
+++ manolo_scraper/tests/test_utils.py
@@ -31,3 +31,39 @@ class TestUtils(unittest.TestCase):
         expected = 'DMI', '803802300'
         result = get_dni(given)
         self.assertEqual(expected, result)
+
+    def test_get_dmi_with_dash(self):
+        given = 'DMI : O-95505'
+        expected = 'DMI', 'O-95505'
+        result = get_dni(given)
+        self.assertEqual(expected, result)
+
+    def test_get_otr(self):
+        given = 'OTR : 0196307704'
+        expected = 'OTR', '0196307704'
+        result = get_dni(given)
+        self.assertEqual(expected, result)
+
+    def test_get_cex(self):
+        given = 'CEX : 1478-2011'
+        expected = 'CEX', '1478-2011'
+        result = get_dni(given)
+        self.assertEqual(expected, result)
+
+    def test_get_cdi(self):
+        given = 'CDI : EA814654'
+        expected = 'CDI', 'EA814654'
+        result = get_dni(given)
+        self.assertEqual(expected, result)
+
+    def test_get_passport(self):
+        given = 'PSP : AI0386093'
+        expected = 'PSP', 'AI0386093'
+        result = get_dni(given)
+        self.assertEqual(expected, result)
+
+    def test_get_brevete(self):
+        given = 'ATG : BREVETE'
+        expected = 'ATG', 'BREVETE'
+        result = get_dni(given)
+        self.assertEqual(expected, result)
