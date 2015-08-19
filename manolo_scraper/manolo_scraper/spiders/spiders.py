8c0b72db6e16fb869e6f43a6374218694cf9f692
Edgar Marca <matiskay@gmail.com>
2015-08-18 23:39:05 -0300

    Fix PCM bug
    PCM has an extra column called SEDE

--- manolo_scraper/manolo_scraper/spiders/spiders.py
+++ manolo_scraper/manolo_scraper/spiders/spiders.py
@@ -78,32 +78,7 @@ class SireviSpider(ManoloBaseSpider):
             data = row.xpath('.//td').extract()
 
             if len(data) > 3:
-                l = ManoloItemLoader(item=ManoloItem(), selector=row)
-
-                l.add_value('institution', self.institution_name)
-                l.add_value('date', date_str)
-
-                l.add_xpath('full_name', './td[2]/text()')
-                l.add_xpath('entity', './td[4]/text()')
-                l.add_xpath('reason', './td[5]/text()')
-                l.add_xpath('host_name', './td[6]/text()')
-                l.add_xpath('title', './td[7]/text()')
-                l.add_xpath('office', './td[8]/text()')
-                l.add_xpath('time_start', './td[9]/text()')
-                l.add_xpath('time_end', './td[10]/text()')
-
-                try:
-                    document_identity = data[2].strip()
-                except IndexError:
-                    document_identity = ''
-
-                if document_identity != '':
-                    id_document, id_number = get_dni(document_identity)
-
-                    l.add_value('id_document', id_document)
-                    l.add_value('id_number', id_number)
-
-                item = l.load_item()
+                item = self.get_item(data, date_str, row)
 
                 item = make_hash(item)
 
@@ -125,3 +100,30 @@ class SireviSpider(ManoloBaseSpider):
 
     def _get_page_url(self, page_number):
         return self.base_url + self.ajax_page_pattern % page_number
+
+    def get_item(self, data, date_str, row):
+        l = ManoloItemLoader(item=ManoloItem(), selector=row)
+
+        l.add_value('institution', self.institution_name)
+        l.add_value('date', date_str)
+        l.add_xpath('full_name', './td[2]/text()')
+        l.add_xpath('entity', './td[4]/text()')
+        l.add_xpath('reason', './td[5]/text()')
+        l.add_xpath('host_name', './td[6]/text()')
+        l.add_xpath('title', './td[7]/text()')
+        l.add_xpath('office', './td[8]/text()')
+        l.add_xpath('time_start', './td[9]/text()')
+        l.add_xpath('time_end', './td[10]/text()')
+
+        try:
+            document_identity = data[2].strip()
+        except IndexError:
+            document_identity = ''
+
+        if document_identity != '':
+            id_document, id_number = get_dni(document_identity)
+
+            l.add_value('id_document', id_document)
+            l.add_value('id_number', id_number)
+
+        return l.load_item()
