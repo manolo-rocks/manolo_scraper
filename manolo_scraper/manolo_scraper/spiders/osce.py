fc745b008f142349dafbddb6a3673e8b7bf7aa51
Edgar Marca <matiskay@gmail.com>
2015-08-18 23:53:56 -0300

    Add Spider for OSCE Spider

--- manolo_scraper/manolo_scraper/spiders/osce.py
+++ manolo_scraper/manolo_scraper/spiders/osce.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 from spiders import SireviSpider
 
+
 class OSCESpider(SireviSpider):
     name = "osce"
 
-    allowed_domains = ["visitas.osce.gob.pe"]
+    allowed_domains = ['visitas.osce.gob.pe']
 
     base_url = 'http://visitas.osce.gob.pe/controlVisitas'
     institution_name = 'osce'
