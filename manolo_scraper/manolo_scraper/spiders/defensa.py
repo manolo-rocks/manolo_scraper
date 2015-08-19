2047f39b380d0617600906886d09ffb0374b1205
Edgar Marca <matiskay@gmail.com>
2015-08-19 19:12:56 -0300

    Defensa Spider now inherits from ManoloBaseSpider and Add test for Defensa Spider

--- manolo_scraper/manolo_scraper/spiders/defensa.py
+++ manolo_scraper/manolo_scraper/spiders/defensa.py
@@ -3,37 +3,30 @@ import datetime
 import logging
 
 import scrapy
-from scrapy import exceptions
 
+from spiders import ManoloBaseSpider
 from ..items import ManoloItem
+from ..item_loaders import ManoloItemLoader
 from ..utils import make_hash, get_dni
 
 
-class DefensaSpider(scrapy.Spider):
-    name = "defensa"
-    allowed_domains = ["mindef.gob.pe"]
-
-    def __init__(self, date_start=None, *args, **kwargs):
-        super(DefensaSpider, self).__init__(*args, **kwargs)
-        self.base_url = "http://www.mindef.gob.pe/visitas/qryvisitas.php"
-
-        self.date_start = date_start
-        if self.date_start is None:
-            raise exceptions.UsageError('Enter start date as spider argument: -a date_start=')
+class DefensaSpider(ManoloBaseSpider):
+    name = 'defensa'
+    allowed_domains = ['mindef.gob.pe']
 
     def start_requests(self):
         d1 = datetime.datetime.strptime(self.date_start, '%Y-%m-%d').date()
-        d2 = datetime.date.today()
+        d2 = datetime.datetime.strptime(self.date_end, '%Y-%m-%d').date()
         delta = d2 - d1
 
         for i in range(delta.days + 1):
-            my_date = d1 + datetime.timedelta(days=i)
-            my_date_str = my_date.strftime("%d/%m/%Y")
-            logging.info("SCRAPING: {}".format(my_date_str))
+            date = d1 + datetime.timedelta(days=i)
+            date_str = date.strftime("%d/%m/%Y")
+            logging.info("SCRAPING: {}".format(date_str))
 
-            params = {'fechaqry': my_date_str}
-            yield scrapy.FormRequest(url=self.base_url, formdata=params,
-                                     meta={'date': my_date_str},
+            params = {'fechaqry': date_str}
+            yield scrapy.FormRequest(url="http://www.mindef.gob.pe/visitas/qryvisitas.php", formdata=params,
+                                     meta={'date': date_str},
                                      dont_filter=True,
                                      callback=self.after_post)
 
