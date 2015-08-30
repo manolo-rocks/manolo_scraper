75d4731e19318f60ac9c607136ceb244aa935191
aniversarioperu <aniversarioperu1@gmail.com>
2015-08-30 16:52:52 +0300

    use generic script, fixes #53

---
+++ run_scraper.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+last_week=$(date --date='7 days ago' +"%Y-%m-%d")
+today=$(date +"%Y-%m-%d")
+
+# edit these path
+spider=$1
+scrapy_path=/home/yoni/manolo_scraper/manolo_scraper
+scrapy_bin=/home/yoni/.virtualenvs/manolo_scraper/bin/scrapy
+scraper_log=/home/yoni/manolo_scraper_logs.txt
+
+cd $scrapy_path
+
+# run using the name of the spider as argument
+$scrapy_bin crawl $spider -a date_start=$last_week -a date_end=$today \
+      >> $scraper_log 2>&1
--- run_scraper_inpe.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-last_week=$(date --date='7 days ago' +"%Y-%m-%d")
-
-cd /data2/ani/projects/aniversario_peru_github/manolo_scraper/manolo_scraper
-
-tsocks /home/ani/.virtualenvs/manolo_scraper/bin/scrapy crawl inpe -a date_start=$last_week \
-      >> /home/ani/Desktop/log 2>&1
