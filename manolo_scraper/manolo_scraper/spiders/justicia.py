e33e21155b74bd640d3160711e9ea60899177d43
aniversarioperu <aniversarioperu1@gmail.com>
2015-09-05 22:22:11 +0300

    fixed and added warnings when time_start, time_end missing
    they will be logged as warning.

--- manolo_scraper/manolo_scraper/spiders/justicia.py
+++ manolo_scraper/manolo_scraper/spiders/justicia.py
@@ -1,9 +1,10 @@
+import logging
+
 from scrapy import FormRequest
 
 from spiders import ManoloBaseSpider
 from ..items import ManoloItem
 from ..item_loaders import ManoloItemLoader
-
 from ..utils import make_hash
 
 
@@ -15,7 +16,35 @@ class JusticiaSpider(ManoloBaseSpider):
     def initial_request(self, date):
         date_str = date.strftime('%d/%m/%Y')
         request = self._request_number_of_pages(date_str)
+        return request
 
+    def _request_number_of_pages(self, date_str):
+        url = self.base_url + '/consulta_paginarBusquedaVisitas'
+
+        request = FormRequest(url=url,
+                              meta={
+                                  'date': date_str,
+                              },
+                              formdata={
+                                  'fechaDesde': date_str,
+                                  'fechaHasta': date_str,
+                                  'paginaActual': '1',
+                                  'visita.visitanteNombres': '',
+                                  'visita.personalNombre': '',
+                                  'visita.oficinaNombre': '',
+                                  'visita.sedeId': '00',
+                                  'visita.ano': '',
+                                  'visita.mes': '',
+                                  'visita.fechaIngreso': '',
+                                  'paginaNueva': '0',
+                                  'visita.visitanteId': '0',
+                                  'visita.personalId': '0',
+                                  'visita.oficinaId': '0',
+                              },
+                              dont_filter=True,
+                              callback=self.parse_initial_request)
+
+        request.meta['date'] = date_str
         return request
 
     def parse_initial_request(self, response):
@@ -30,6 +59,33 @@ class JusticiaSpider(ManoloBaseSpider):
                 request = self._request_page(date_str, page, self.parse)
 
                 yield request
+
+    def _request_page(self, date_str, page, callback):
+        url = self.base_url + '/consulta_buscarVisitas'
+
+        request = FormRequest(url=url,
+                              meta={
+                                  'date': date_str,
+                              },
+                              formdata={
+                                  'fechaDesde': date_str,
+                                  'fechaHasta': date_str,
+                                  'paginaActual': str(page),
+                                  'visita.visitanteNombres': '',
+                                  'visita.personalNombre': '',
+                                  'visita.oficinaNombre': '',
+                                  'visita.sedeId': '00',
+                                  'visita.ano': '',
+                                  'visita.mes': '',
+                                  'visita.fechaIngreso': '',
+                                  'paginaNueva': str(page),
+                                  'visita.visitanteId': '0',
+                                  'visita.personalId': '0',
+                                  'visita.oficinaId': '0',
+                              },
+                              dont_filter=True,
+                              callback=callback)
+        return request
 
     def parse(self, response):
         rows = response.xpath('//table/tr')
@@ -58,72 +114,22 @@ class JusticiaSpider(ManoloBaseSpider):
 
             time_start_time_end = time_start_time_end.split('-')
 
+            warnings = []
             try:
                 l.add_value('time_start', time_start_time_end[0])
+            except IndexError:
+                warnings.append("No time_start for item: ")
+
+            try:
                 l.add_value('time_end', time_start_time_end[1])
             except IndexError:
-                pass
+                warnings.append("No time_end for item: ")
 
             item = l.load_item()
-
             item = make_hash(item)
 
+            if warnings:
+                for i in warnings:
+                    logging.warning("{0}{1}".format(i, item))
+
             yield item
-
-    def _request_page(self, date_str, page, callback):
-        url = self.base_url + '/consulta_buscarVisitas'
-
-        request = FormRequest(url=url,
-                              meta={
-                                  'date': date_str,
-                                  },
-                              formdata={
-                                  'fechaDesde': date_str,
-                                  'fechaHasta': date_str,
-                                  'paginaActual': str(page),
-                                  'visita.visitanteNombres': '',
-                                  'visita.personalNombre': '',
-                                  'visita.oficinaNombre': '',
-                                  'visita.sedeId': '00',
-                                  'visita.ano': '',
-                                  'visita.mes': '',
-                                  'visita.fechaIngreso': '',
-                                  'paginaNueva': str(page),
-                                  'visita.visitanteId': '0',
-                                  'visita.personalId': '0',
-                                  'visita.oficinaId': '0',
-                                  },
-                              dont_filter=True,
-                              callback=callback)
-
-        return request
-
-    def _request_number_of_pages(self, date_str):
-        url = self.base_url + '/consulta_paginarBusquedaVisitas'
-
-        request = FormRequest(url=url,
-                              meta={
-                                  'date': date_str,
-                                  },
-                              formdata={
-                                  'fechaDesde': date_str,
-                                  'fechaHasta': date_str,
-                                  'paginaActual': '1',
-                                  'visita.visitanteNombres': '',
-                                  'visita.personalNombre': '',
-                                  'visita.oficinaNombre': '',
-                                  'visita.sedeId': '00',
-                                  'visita.ano': '',
-                                  'visita.mes': '',
-                                  'visita.fechaIngreso': '',
-                                  'paginaNueva': '0',
-                                  'visita.visitanteId': '0',
-                                  'visita.personalId': '0',
-                                  'visita.oficinaId': '0',
-                                  },
-                              dont_filter=True,
-                              callback=self.parse_initial_request)
-
-        request.meta['date'] = date_str
-
-        return request
