5e32a10f3fe9c4b3ad2953fbfa57bae589099fc1
carlosp420 <mycalesis@gmail.com>
2016-10-14 15:15:14 -0500

    fixed mincu

--- manolo_scraper/manolo_scraper/spiders/mincu.py
+++ manolo_scraper/manolo_scraper/spiders/mincu.py
@@ -5,8 +5,8 @@ from spiders import SireviSpider
 class MincuSpider(SireviSpider):
     name = 'mincu'
 
-    allowed_domains = ['visitas.mcultura.gob.pe']
+    allowed_domains = ['visitas.cultura.gob.pe']
 
-    base_url = 'http://visitas.mcultura.gob.pe'
+    base_url = 'http://visitas.cultura.gob.pe'
 
     institution_name = 'mincu'
