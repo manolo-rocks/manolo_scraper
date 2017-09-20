65dfc6c75189cb619cc82e11d2cd72a581c7dbbc
carlosp420 <mycalesis@gmail.com>
2017-09-19 22:13:40 -0500

    updated url for minagri

--- manolo_scraper/manolo_scraper/spiders/minagr.py
+++ manolo_scraper/manolo_scraper/spiders/minagr.py
@@ -5,8 +5,8 @@ from spiders import SireviSpider
 class MinagrSpider(SireviSpider):
     name = 'minagr'
 
-    allowed_domains = ['sistemas.minag.gob.pe']
+    allowed_domains = ['minagri.gob.pe']
 
-    base_url = 'http://sistemas.minag.gob.pe/visitas/controlVisitas'
+    base_url = 'http://visitas.minagri.gob.pe/visitas/controlVisitas'
 
     institution_name = 'minagr'
