fa255c133570f794a2ade55232c4f0a3007b4348
carlosp420 <mycalesis@gmail.com>
2017-10-14 11:43:43 -0500

    fixed defensa

--- manolo_scraper/manolo_scraper/spiders/defensa.py
+++ manolo_scraper/manolo_scraper/spiders/defensa.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import datetime
+
 import scrapy
 
 from spiders import ManoloBaseSpider
@@ -9,56 +11,51 @@ from ..utils import make_hash, get_dni
 
 class DefensaSpider(ManoloBaseSpider):
     name = 'defensa'
-    allowed_domains = ['mindef.gob.pe']
-    base_url = 'https://www.mindef.gob.pe/visitas/qryvisitas.php'
+    allowed_domains = ['transparencia.gob.pe']
+    base_url = 'http://200.37.34.66/visitas/visitas_transparencia.php'
 
     def initial_request(self, date):
+        # There is no pagination in Defensa
         date_str = date.strftime("%d/%m/%Y")
-        params = {'fechaqry': date_str}
+        params = {'fecha': date_str}
+        print("Params {}".format(params))
         return scrapy.FormRequest(
             url=self.base_url,
             formdata=params,
             meta={'date': date_str},
             dont_filter=True,
-            callback=self.after_post,
+            callback=self.parse,
         )
 
-    def after_post(self, response):
-        # send requests based on pagination
-        pages = response.xpath('//select[@id="pag_actual"]//option/text()').extract()
-        for page in pages:
-            params = {
-                'fechaqry': response.meta['date'],
-                'pag_actual': page,
-            }
-            yield scrapy.FormRequest(
-                url=self.base_url,
-                formdata=params,
-                meta={'date': response.meta['date']},
-                dont_filter=True,
-                callback=self.parse,
-            )
+    def start_requests(self):
+        # Defensa returns all visits from starting date to present, no need
+        # to iterate over each day
+        d1 = datetime.datetime.strptime(self.date_start, '%Y-%m-%d').date()
+
+        print("SCRAPING FROM: {} to present".format(d1))
+        yield self.initial_request(d1)
 
     def parse(self, response):
-        date = self.get_date_item(response.meta['date'], '%d/%m/%Y')
         rows = response.xpath('//tr')
         for row in rows:
-            data = row.xpath('.//td[@class="clsdetalle"]')
+            data = row.xpath('.//td[@class="clsgriddata2"]')
 
             if len(data) > 5:
                 l = ManoloItemLoader(item=ManoloItem(), selector=row)
 
                 l.add_value('institution', 'defensa')
-                l.add_value('date', date)
+                l.add_xpath('date', './/td[2]/text()')
 
-                l.add_xpath('full_name', './/td[@class="clsdetalle"][2]/text()')
-                l.add_xpath('entity', './/td[@class="clsdetalle"][4]/text()')
-                l.add_xpath('reason', './/td[@class="clsdetalle"][5]/text()')
-                l.add_xpath('host_name', './/td[@class="clsdetalle"][6]/text()')
-                l.add_xpath('time_start', './/td[@class="clsdetalle"][7]/text()')
-                l.add_xpath('time_end', './/td[@class="clsdetalle"][8]/text()')
+                l.add_xpath('full_name', './/td[3]/text()')
+                l.add_xpath('entity', './/td[5]/text()')
+                l.add_xpath('reason', './/td[6]/text()')
+                l.add_xpath('host_name', './/td[7]/text()')
+                l.add_xpath('title', './/td[8]/text()')
+                l.add_xpath('office', './/td[9]/text()')
+                l.add_xpath('time_start', './/td[10]/text()')
+                l.add_xpath('time_end', './/td[11]/text()')
 
-                id_document, id_number = get_dni(data[2].xpath('text()').extract_first(default=''))
+                id_document, id_number = get_dni(data[3].xpath('text()').extract_first(default=''))
 
                 l.add_value('id_document', id_document)
                 l.add_value('id_number', id_number)
